fb4f01accd734f1d383d9e9b69f3c88e55e0e993
abdelrhman1812 <abdelrhmanali1812@gmail.com>
2024-11-19 20:06:51 +0200

    fix screen mobile

--- src/hooks/useResponsive.jsx
+++ src/hooks/useResponsive.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 
 const useResponsive = () => {
-  const [showForm, setShowForm] = useState(true);
+  const [showForm, setShowForm] = useState(false); // افتراضياً يكون الفورم مخفي
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 991);
 
   const handleResize = () => {
     setIsMobile(window.innerWidth <= 991);
+    // عند تغيير حجم الشاشة لأكبر من 991، خلي الفورم دايمًا يظهر
     if (window.innerWidth > 991) {
       setShowForm(true);
     }
@@ -20,6 +21,7 @@ const useResponsive = () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+
   return { isMobile, showForm, setShowForm };
 };
 
